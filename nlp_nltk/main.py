177c31017ec231f4b205c3743948a186e3d15288
vplechuc <vplechuc@gmail.com>
2020-12-29 12:39:05 -0300

    stemming

--- nlp_nltk/main.py
+++ nlp_nltk/main.py
@@ -1,9 +1,10 @@
 from nltk.tokenize import sent_tokenize
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
 import string
 
-text = "Computers don't speak English. So, we've to learn C, C++, ,C#, Java, Python and the like! Yay!. Or may we should use another tool"
+text = "Computers don't speak or speaking in English. So, we've to learn, or learning C, C++, ,C#, Java, Python and the like! Yay!. Or may we should use another tool"
 
 #tokenize sentence
 sentences = sent_tokenize(text)
@@ -25,3 +26,10 @@ punctuations = list(string.punctuation)
 #print(punctuations)
 words = [word for word in words if word not in punctuations]
 print(len(words), "words without stopwords and punctuations:", words)
+
+#stemming stemm the root of the word
+ps = PorterStemmer()
+
+stemmed_example = [ps.stem(word) for word in words ] # remember to tokenize the text first
+
+print (f'Stemming -> {stemmed_example}')
