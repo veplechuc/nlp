06485bfa9fe1a1efb45906e49338670c193cdf62
vplechuc <vplechuc@gmail.com>
2020-12-29 17:20:54 -0300

    tagging

--- nlp_nltk/main.py
+++ nlp_nltk/main.py
@@ -1,7 +1,9 @@
-from nltk.tokenize import sent_tokenize
-from nltk.tokenize import word_tokenize
+from nltk import pos_tag
+from nltk.tokenize import word_tokenize, sent_tokenize,PunktSentenceTokenizer
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
+from nltk.corpus import state_union
+
 import string
 
 text = "Computers don't speak or speaking in English. So, we've to learn, or learning C, C++, ,C#, Java, Python and the like! Yay!. Or may we should use another tool"
@@ -33,3 +35,27 @@ ps = PorterStemmer()
 stemmed_example = [ps.stem(word) for word in words ] # remember to tokenize the text first
 
 print (f'Stemming -> {stemmed_example}')
+
+#tagging
+# need to have installed
+# nltk.download('averaged_perceptron_tagger')
+# nltk.download('state_union')
+train_text = state_union.raw("2005-GWBush.txt")
+sample_text = state_union.raw("2006-GWBush.txt")
+
+custom_sent_tokenizer = PunktSentenceTokenizer(train_text)
+
+tokenized = custom_sent_tokenizer.tokenize(sample_text)
+
+def process_content():
+    try:
+        for i in tokenized[:5]:
+            words = word_tokenize(i)
+            tagged = pos_tag(words)
+            print(tagged)
+
+    except Exception as e:
+        print(str(e))
+
+
+process_content()
