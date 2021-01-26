51337d614aae47641c205f42a815bd8d564f2630
vplechuc <vplechuc@gmail.com>
2021-01-26 15:41:00 -0300

    adding wordnet

--- nlp_nltk/main.py
+++ nlp_nltk/main.py
@@ -1,8 +1,11 @@
-from nltk import pos_tag, RegexpParser, ne_chunk
+import nltk 
 from nltk.tokenize import word_tokenize, sent_tokenize,PunktSentenceTokenizer
-from nltk.corpus import stopwords, state_union
-from nltk.stem import PorterStemmer
+from nltk.corpus import stopwords, state_union, wordnet
+from nltk.stem import PorterStemmer, WordNetLemmatizer
 import string
+
+# where nltk is located
+print(f'NLTK file location {nltk.__file__}')
 
 text = "Computers don't speak or speaking in English. So, we've to learn, or learning C, C++, ,C#, Java, Python and the like! Yay!. Or may we should use another tool"
 
@@ -40,6 +43,7 @@ print (f'Stemming -> {stemmed_example}')
 # nltk.download('state_union')
 # nltk.download('maxent_ne_chunker')
 # nltk.download('words')
+# nltk.download('wordnet')
 train_text = state_union.raw("2005-GWBush.txt")
 sample_text = state_union.raw("2006-GWBush.txt")
 
@@ -51,7 +55,7 @@ def process_content():
     try:
         for i in tokenized[:5]:
             words = word_tokenize(i)
-            tagged = pos_tag(words)
+            tagged = nltk.pos_tag(words)
             #using chunck
             # print(tagged)
             # chunkGram = r"""Chunk: {<RB.?>*<VB.?>*<NNP>+<NN>?}"""
@@ -61,13 +65,58 @@ def process_content():
             # for subtree in chunked.subtrees(filter=lambda t: t.label() == 'Chunk'):
             #     print(subtree)
             # chunked.draw()     
-            namedEnt = ne_chunk(tagged, binary=True)
+            namedEnt = nltk.ne_chunk(tagged, binary=True)
             namedEnt.draw()
 
     except Exception as e:
         print(str(e))
 
-process_content()
+
+lemmatizer = WordNetLemmatizer()
+
+def compare_wordnet():
+    w1 = wordnet.synset('ship.n.01')
+    w2 = wordnet.synset('boat.n.01')
+    print(w1.wup_similarity(w2))
+    w1 = wordnet.synset('ship.n.01')
+    w2 = wordnet.synset('cat.n.01')
+    print(w1.wup_similarity(w2))
+
+
+# print(lemmatizer.lemmatize("cats"))
+# print(lemmatizer.lemmatize("cacti"))
+# print(lemmatizer.lemmatize("geese"))
+# print(lemmatizer.lemmatize("rocks"))
+# print(lemmatizer.lemmatize("python"))
+# print(lemmatizer.lemmatize("better", pos="a"))
+# print(lemmatizer.lemmatize("best", pos="a"))
+# print(lemmatizer.lemmatize("run"))
+# print(lemmatizer.lemmatize("run",'v'))
+
+##WORDNET
+syns = wordnet.synsets("program")
+
+print(syns[0].name())
+synonyms = []
+antonyms = []
+
+for syn in wordnet.synsets("good"):
+    for l in syn.lemmas():
+        synonyms.append(l.name())
+        if l.antonyms():
+            antonyms.append(l.antonyms()[0].name())
+
+print(set(synonyms))
+print(set(antonyms))
+
+#process_content()
+
+# Let's compare the noun of "ship" and "boat:"
+compare_wordnet()
+
+
+
+
 
 ############COMMENTS
 # NE Type and Examples
