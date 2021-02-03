9f733d86b39213a60d0d08232ac07663710b8719
vplechuc <vplechuc@gmail.com>
2021-02-03 13:14:59 -0300

    Combining classifier algorithms, and adding loguru

--- nlp_nltk/text_clasification.py
+++ nlp_nltk/text_clasification.py
@@ -1,3 +1,4 @@
+from loguru import logger
 import nltk
 import random
 from nltk.corpus import movie_reviews
@@ -9,7 +10,23 @@ from sklearn.naive_bayes import MultinomialNB,BernoulliNB
 from sklearn.linear_model import LogisticRegression,SGDClassifier
 from sklearn.svm import SVC, LinearSVC, NuSVC
 
+from nltk.classify import ClassifierI
+from statistics import mode
 
+class VoteClassifier(ClassifierI):
+    def __init__(self, *classifiers):
+        self._classifiers = classifiers
+
+    def classify(self, features):
+        votes = [c.classify(features) for c in self._classifiers]
+        return mode(votes)
+
+    def confidence(self, features):
+        votes = []
+        votes = [c.classify(features) for c in self._classifiers]
+        choice_votes = votes.count(mode(votes))
+        conf = choice_votes / len(votes)
+        return conf
 
 "remember --> nltk.download('movie_reviews')"
 
@@ -33,11 +50,13 @@ def find_features(document):
 
 #save model
 def save_model(model, file_name):
+    logger.info("Saving model...")
     with open(f'{file_name}.pickle', "wb") as save_classifier:
         pickle.dump(model, save_classifier)
     
 #load the model saved
 def load_model(file_name):
+    logger.info("Loading model...")
     with open(f'{file_name}.pickle', "rb") as classifier_f:
         classifier = pickle.load(classifier_f)
     return classifier
@@ -52,6 +71,7 @@ all_words = nltk.FreqDist(all_words)
 # word = 'bad'
 # print(f'how many times {word} appears on the text --> {all_words[word]}')
 
+logger.info("Create word features...")
 word_features = list(all_words.keys())[:3000]
 
 # print((find_features(movie_reviews.words('neg/cv000_29416.txt'))))
@@ -68,10 +88,10 @@ testing_set = featuresets[1900:]
 #classifier = nltk.NaiveBayesClassifier.train(training_set)
 
 # now call the saved method comment if this is the first time you run this program
-classifier = load_model('naive_bayes2')
+NB_classifier = load_model('naive_bayes2')
 
 
-print("Classifier NaiveBayes accuracy percent:",(nltk.classify.accuracy(classifier, testing_set))*100)
+print("Classifier NaiveBayes accuracy percent:",(nltk.classify.accuracy(NB_classifier, testing_set))*100)
 # what the most valuable words are when it comes to positive or negative reviews:
 # classifier.show_most_informative_features(15)
 # save_model(classifier, 'naive_bayes2')
@@ -83,7 +103,19 @@ models = [MultinomialNB, BernoulliNB, LogisticRegression,
 def classify(models):
     for model in models:
         classifier = SklearnClassifier(model())
+        classifiers.append(classifier)
         classifier.train(training_set)
         print(f'{model.__name__}_classifier accuracy percent:', (nltk.classify.accuracy(classifier, testing_set))*100)
 
+#adding the first classifier
+classifiers = [NB_classifier]
+
+logger.info("print accuracy...")
 classify(models)
+
+voted_classifier = VoteClassifier(*classifiers)
+
+print("voted_classifier accuracy percent:", (nltk.classify.accuracy(voted_classifier, testing_set))*100)
+for x in range(0,5):
+    print("Classification:", voted_classifier.classify(testing_set[x][0]), 
+            "Confidence %:",voted_classifier.confidence(testing_set[x][0])*100)
