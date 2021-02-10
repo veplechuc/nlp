3f724d1e4ebb7d16a3f6b75b5aff12628b11474b
vplechuc <vplechuc@gmail.com>
2021-02-10 17:43:00 -0300

    more refactoring

--- nlp_nltk/text_clasification.py
+++ nlp_nltk/text_clasification.py
@@ -1,17 +1,15 @@
 from vote_classifier import VoteClassifier
-from utils import save_model, load_model
+from utils import save_model, load_model, get_documents, all_words_freq
 from loguru import logger
-import nltk
-import random
-from nltk.corpus import movie_reviews
 
+import nltk
 from nltk.classify.scikitlearn import SklearnClassifier
+from nltk.classify import ClassifierI
 
 from sklearn.naive_bayes import MultinomialNB, BernoulliNB
 from sklearn.linear_model import LogisticRegression, SGDClassifier
 from sklearn.svm import SVC, LinearSVC, NuSVC
 
-from nltk.classify import ClassifierI
 from statistics import mode
 
 "remember --> nltk.download('movie_reviews')"
@@ -29,29 +27,6 @@ models = [
 
 TOP_WORDS = 3000
 ELEMENTS = 1900
-
-
-def get_documents():
-    """In each category (we have pos or neg), take all of the file IDs (each review has its own ID),
-    then store the word_tokenized version (a list of words) for the file ID, followed by the positive or negative label in one big list
-    """
-    documents = [
-        (list(movie_reviews.words(fileid)), category)
-        for category in movie_reviews.categories()
-        for fileid in movie_reviews.fileids(category)
-    ]
-
-    return random.shuffle(documents)
-
-
-def all_words_freq():
-    all_words = [w.lower() for w in movie_reviews.words()]
-    all_words = nltk.FreqDist(all_words)
-    # print(all_words.most_common(15))
-    # word = 'bad'
-    # print(f'how many times {word} appears on the text --> {all_words[word]}')
-    return all_words
-
 
 def word_features():
     logger.info("Create word features from movie_reviews...")
@@ -74,9 +49,6 @@ def find_features(document):
     return features
 
 
-# print(documents[1])
-
-
 def feature_set():
     featuresets = [
         (find_features(rev), category) for (rev, category) in get_documents()
@@ -84,47 +56,7 @@ def feature_set():
     return featuresets
 
 
-# set that we'll train our classifier with
-training_set = feature_set()[:ELEMENTS]
-
-# set that we'll test against.
-testing_set = feature_set()[ELEMENTS:]
-
-# uncomment this line if you dont have the model saved
-# classifier = nltk.NaiveBayesClassifier.train(training_set)
-
-# now call the saved method --- comment if this is the first time you run this program
-NB_classifier = load_model("naive_bayes2")
-
-
-print(
-    "Classifier NaiveBayes accuracy percent:",
-    (nltk.classify.accuracy(NB_classifier, testing_set)) * 100,
-)
-# what the most valuable words are when it comes to positive or negative reviews:
-# classifier.show_most_informative_features(15)
-# save_model(classifier, 'naive_bayes2')
-
-# @njit(parallel=True)
-def classify(models):
-    for model in models:
-        classifier = SklearnClassifier(model())
-        classifiers.append(classifier)  # adding the rest of classifiers
-        classifier.train(training_set)
-        print(
-            f"{model.__name__}_classifier accuracy percent:",
-            (nltk.classify.accuracy(classifier, testing_set)) * 100,
-        )
-
-
-# adding the first classifier
-classifiers = [NB_classifier]
-
-logger.info("print accuracy...")
-classify(models)
-
-
-def list_confidence():
+def list_confidence(classifiers, training_set, testing_set):
     voted_classifier = VoteClassifier(*classifiers)
     logger.info(
         "voted_classifier accuracy percent: ",
@@ -138,3 +70,54 @@ def list_confidence():
             "Confidence %:",
             voted_classifier.confidence(testing_set[x][0]) * 100,
         )
+
+# @njit(parallel=True)
+def classify(models, classifiers, training_set, testing_set):
+    for model in models:
+        classifier = SklearnClassifier(model())
+        classifiers.append(classifier)  # adding the rest of classifiers
+        classifier.train(training_set)
+        print(
+            f"{model.__name__}_classifier accuracy percent:",
+            (nltk.classify.accuracy(classifier, testing_set)) * 100,
+        )
+
+def naive_bayes_classifier(training_set):
+    # uncomment this line if you dont have the model saved
+    return nltk.NaiveBayesClassifier.train(training_set)
+
+
+def proccess():
+
+    # set that we'll train our classifier with
+    training_set = feature_set()[:ELEMENTS]
+
+    # set that we'll test against.
+    testing_set = feature_set()[ELEMENTS:]
+
+    if not exists_file:
+        # uncomment this line if you dont have the model saved
+        NB_classifier = nltk.NaiveBayesClassifier.train(training_set)
+        save_model(NB_classifier, 'naive_bayes2')
+
+    # now call the saved method 
+    NB_classifier = load_model("naive_bayes2")
+
+    print(
+        "Classifier NaiveBayes accuracy percent:",
+        (nltk.classify.accuracy(NB_classifier, testing_set)) * 100,
+    )
+    # what the most valuable words are when it comes to positive or negative reviews:
+    # classifier.show_most_informative_features(15)
+
+
+    # adding the first classifier
+    classifiers = [NB_classifier]
+    logger.info("print accuracy...")
+    classify(models, classifiers, training_set, testing_set)
+
+    list_confidence(classifiers, training_set, testing_set)
+
+
+if __name__ == "__main__":
+    proccess()
--- nlp_nltk/utils.py
+++ nlp_nltk/utils.py
@@ -1,5 +1,8 @@
 from loguru import logger
 import pickle
+from nltk.corpus import movie_reviews
+from nltk import FreqDist
+import random
 
 def save_model(model, file_name):
     logger.info("Saving model...")
@@ -12,3 +15,23 @@ def load_model(file_name):
     with open(f"{file_name}.pickle", "rb") as classifier_f:
         classifier = pickle.load(classifier_f)
     return classifier
+
+def get_documents():
+    """In each category (we have pos or neg), take all of the file IDs (each review has its own ID),
+    then store the word_tokenized version (a list of words) for the file ID, followed by the positive or negative label in one big list
+    """
+    documents = [
+        (list(movie_reviews.words(fileid)), category)
+        for category in movie_reviews.categories()
+        for fileid in movie_reviews.fileids(category)
+    ]
+
+    return random.shuffle(documents)
+
+def all_words_freq():
+    all_words = [w.lower() for w in movie_reviews.words()]
+    all_words = FreqDist(all_words)
+    # print(all_words.most_common(15))
+    # word = 'bad'
+    # print(f'how many times {word} appears on the text --> {all_words[word]}')
+    return all_words
